04359ed469c1b4c48cf131c35aea28bd23c8283d
Hugo Caminada <caminada@redkiwi.nl>
2021-04-30 15:43:47 +0200

    Alles

--- pages/index.js
+++ pages/index.js
@@ -13,56 +13,29 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          CONRAD SOUNDSYSTEM
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.image}
+            src="/conrad.jpg"
+            alt="picture of Conrad"
+            width={450}
+            height={450}
+          />
         </div>
-      </main>
 
+        <div className={styles.mixcloud}>
+          <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fconrad-soundsystem%2Fconrad-soundsystem-clubmusic-in-2020%2F" frameborder="0" />
+          <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fooradio%2F020%2F" frameborder="0" />
+          <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fconrad-soundsystem%2Fconrad-soundsystem-breaks-vinyl-mix%2F" frameborder="0" />
+        </div>
+
+      </main>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          <a href="mailto:general@conradsoundsystem.com">general@conradsoundsystem.com</a>
+          <p>Conrad Soundsystem are Hugo Eno and David Lieffering</p>
       </footer>
     </div>
   )
