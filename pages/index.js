86d6e8281957eacd3b20471b0d356ded02c6b45a
Hugo Caminada <caminada@redkiwi.nl>
2021-05-19 11:09:01 +0200

    Added SC instead of YT

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           CONRAD SOUNDSYSTEM
         </h1>
 
-        {/* <div className={styles.imageContainer}>
+        <div className={styles.imageContainer}>
           <Image
             className={styles.image}
             src="/conrad.jpg"
@@ -25,14 +25,14 @@ export default function Home() {
             width={500}
             height={500}
           />
-        </div> */}
-
-        <div className={styles.youtube}>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/PgLuOULqQhg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
+        {/* <div className={styles.youtube}>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/PgLuOULqQhg?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div> */}
+
         <div className={styles.mixcloud}>
-          {/* <iframe width="100%" height="115" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/983622586%3Fsecret_token%3Ds-L27yo9oFBto&color=%236c6c6c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe> */}
+          <iframe width="100%" height="115" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1051341979&color=%236c6c6c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
           <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fconrad-soundsystem%2Fconrad-soundsystem-clubmusic-in-2020%2F" frameborder="0" />
           <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fooradio%2F020%2F" frameborder="0" />
           <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fconrad-soundsystem%2Fconrad-soundsystem-breaks-vinyl-mix%2F" frameborder="0" />
